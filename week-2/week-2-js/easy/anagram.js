dba1304df5a6294bc14eada5771fa07c1592b9f9
Vishal-Parihar <vishalparihar1008@gmail.com>
2024-09-02 00:54:45 +0530

    week2 assignments

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -4,8 +4,22 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
-function isAnagram(str1, str2) {
 
+function isAnagram(str1, str2) {
+// const arr1 = str1.toLowerCase().split("");  //vis = ['v','i','s']  converts to array
+// arr1.sort(); //['i','s','v']
+// const sorted1 = arr1.join("");     //isv
+
+// const arr2 = str2.toLowerCase().split("");
+// arr2.sort();
+// let sorted2 = arr2.join("");
+
+
+const sorted1 = str1.toLowerCase().split("").sort().join("");
+const sorted2 = str2.toLowerCase().split("").sort().join("");
+
+if(sorted1 ==sorted2) return true;
+else return false;
 }
 
 module.exports = isAnagram;

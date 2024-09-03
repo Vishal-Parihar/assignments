eaaf1bf142e2465c21b12cf87178d7e2843e3188
Vishal-Parihar <vishalparihar1008@gmail.com>
2024-09-03 22:28:35 +0530

    week-2

--- week-2/week-2-js/medium/palindrome.js
+++ week-2/week-2-js/medium/palindrome.js
@@ -3,6 +3,8 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
+
+// The filter() method of Array instances creates a shallow copy of a portion of a given array, filtered down to just the elements from the given array that pass the test implemented by the provided function.\
 function isPalindrome(str) {
   return true;
 }
